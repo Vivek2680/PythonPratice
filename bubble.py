4aaaae53b26d6ae81589e57e739af26cd01b0702
vivek kushwaha <vivekkushwaha@viveks-MacBook-Air.local>
2026-05-20 17:19:02 +0530

    bubble sort

---
+++ bubble.py
@@ -0,0 +1,16 @@
+my_list = [8,10,6,2,4]
+# count = 0
+# for i in range(len(my_list)):
+   
+#     swapped = False 
+#     for j in range((len(my_list))-1-i):
+#         count += 1 
+#         if my_list[j] > my_list[j+1]:
+#             my_list[j],my_list[j+1] = my_list[j+1],my_list[j]
+#             swapped = True
+#             if not swapped:
+#                 break
+# print(my_list)
+# print(count)
+my_list.sort()
+print(my_list)
