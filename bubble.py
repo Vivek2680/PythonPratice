7e4a47b0ebbe7a711eec71407656e863922b1ad8
vivek kushwaha <vivekkushwaha@viveks-MacBook-Air.local>
2026-05-21 16:47:30 +0530

    list

--- bubble.py
+++ bubble.py
@@ -12,5 +12,7 @@ my_list = [8,10,6,2,4]
 #                 break
 # print(my_list)
 # print(count)
-my_list.sort()
-print(my_list)
+# my_list.sort()
+# print(my_list)
+# my_list.reverse()
+# print(my_list)
---
+++ repetation.py
@@ -0,0 +1,29 @@
+# row = []
+# for ic in range(8):
+#     row.append("WHITE_PAWN")
+# print(row)
+
+# row = ["WHITE_PAWN" for i in range(8)]
+# print(row)
+
+# sqares = [x**2 for x in range(1,11)]
+# print(sqares)
+
+# twos = [2**index for index in range(8)]
+# print(twos)
+
+# sqares = [x**2 for x in range(1,11)]
+# odd = [f"{x} is odd num" for x in sqares if x % 2 != 0] # x % 2 != 0 give output only odd
+# print(odd)
+
+#-----------------------------------------------------------------------
+
+# 2D list
+
+# my_list = []
+# for i in range(8):
+#     row = ["Vivek" for i in range(8)]
+#     my_list.append(row)
+# for element in my_list:
+#     print(element)
+# print(len(my_list))
